ff6d28f0fb3990028ce221eec5e4e923d1befd8d
Deep <gagandeepsandhu@protonmail.com>
2024-04-22 01:05:36 -0400

    wip form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import Form from './components/Form'
 
+const INITIAL_FORM_DATA = {
+    initial_investment: null,
+    annual_investment: null,
+    expected_return: null,
+    duration: null,
+}
+
 function App() {
+    const [form_values, set_form_values] = useState(INITIAL_FORM_DATA)
+
+    const update_form_value = (type, value) => {
+        set_form_values((prev_values) => {
+            return {
+                ...prev_values,
+                [type]: value,
+            }
+        })
+    }
+
     return (
         <>
             <header id='header'>
                 <img src='/investment-calculator-logo.png' />
                 <h1>React Investment Calculator</h1>
             </header>
-            <Form />
+            <Form on_update_form_value={update_form_value} />
         </>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,60 @@
-const Form = () => {
+import { useState } from 'react'
+
+const Form = ({ on_update_form_value }) => {
+    const [initial_investment, set_initial_investment] = useState()
+    const [annual_investment, set_annual_investment] = useState()
+    const [expected_return, set_expected_return] = useState()
+    const [duration, set_duration] = useState()
+
     return (
         <div id='user-input'>
             <div className='input-group'>
-                <label htmlFor=''>Initial Investment</label>
-                <input type='number' />
+                <label htmlFor='initial_investment'>
+                    Initial Investment
+                    <input
+                        type='number'
+                        id='initial_investment'
+                        value={initial_investment}
+                        onChange={(event) =>
+                            set_initial_investment(event.target.value)
+                        }
+                    />
+                </label>
+                <label htmlFor='annual_investment'>
+                    Annual Investment
+                    <input
+                        type='number'
+                        id='annual_investment'
+                        value={annual_investment}
+                        onChange={(event) =>
+                            set_annual_investment(event.target.value)
+                        }
+                    />
+                </label>
             </div>
             <div className='input-group'>
-                <label htmlFor=''>Annual Investment</label>
-                <input type='number' />
-            </div>
-            <div className='input-group'>
-                <label htmlFor=''>Expected Return</label>
-                <input type='number' />
-            </div>
-            <div className='input-group'>
-                <label htmlFor=''>Duration</label>
-                <input type='number' />
+                <label htmlFor='expected_return'>
+                    Expected Return
+                    <input
+                        type='number'
+                        id='expected_return'
+                        value={expected_return}
+                        onChange={(event) =>
+                            set_expected_return(event.target.value)
+                        }
+                    />
+                </label>
+                <label htmlFor='duration_input'>
+                    Duration
+                    <input
+                        type='number'
+                        id='duration_input'
+                        value={duration}
+                        onChange={(event) =>
+                            set_duration(event.target.value)
+                        }
+                    />
+                </label>
             </div>
         </div>
     )
